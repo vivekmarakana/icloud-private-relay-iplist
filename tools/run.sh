ed0919240c229a2235bdb0328d3f7c0a878b7e13
Vivek Marakana <vivek.marakana@gmail.com>
2024-10-17 00:15:53 +0530

    no simple ips

--- tools/run.sh
+++ tools/run.sh
@@ -7,9 +7,9 @@ curl -sL "https://mask-api.icloud.com/egress-ip-ranges.csv" | cut -d ',' -f 1 > 
   wc -l egress-ip-ranges.txt && \
   wc -l ipv4-only.txt && \
   wc -l ipv6-only.txt && \
-  cidr-merger -eo ip-ranges.txt egress-ip-ranges.txt && \
-  cidr-merger -eo ipv4/ipv4-ranges.txt ipv4-only.txt && \
-  cidr-merger -eo ipv6/ipv6-ranges.txt ipv6-only.txt && \
+  cidr-merger -eos ip-ranges.txt egress-ip-ranges.txt && \
+  cidr-merger -eos ipv4/ipv4-ranges.txt ipv4-only.txt && \
+  cidr-merger -eos ipv6/ipv6-ranges.txt ipv6-only.txt && \
   wc -l ip-ranges.txt && \
   wc -l ipv4/ipv4-ranges.txt && \
   wc -l ipv6/ipv6-ranges.txt
